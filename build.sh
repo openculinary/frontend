a043f4d522cc70a4cf99ff87eed9b2f7ec0acdc7
James Addison <james@reciperadar.com>
2019-09-19 15:17:21 -0700

    Squash and remove container during build

--- build.sh
+++ build.sh
@@ -4,6 +4,5 @@ IMAGE_COMMIT=$(git rev-parse --short HEAD)
 container=$(buildah from docker.io/library/nginx:latest)
 buildah copy ${container} 'html' '/usr/share/nginx/html'
 buildah config --port 80 --entrypoint '/usr/sbin/nginx -g "daemon off;"' ${container}
-buildah commit ${container} ${IMAGE_NAME}
+buildah commit --squash --rm ${container} ${IMAGE_NAME}
 buildah tag ${IMAGE_NAME} ${IMAGE_COMMIT}
-buildah delete ${container}
