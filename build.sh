c3b528c7c91c56a7cf2ae495c552536a36331d96
James Addison <james@reciperadar.com>
2019-09-19 15:17:17 -0700

    Image build script

---
+++ build.sh
@@ -0,0 +1,9 @@
+IMAGE_NAME='frontend'
+IMAGE_COMMIT=$(git rev-parse --short HEAD)
+
+container=$(buildah from docker.io/library/nginx:latest)
+buildah copy ${container} 'html' '/usr/share/nginx/html'
+buildah config --port 80 --entrypoint '/usr/sbin/nginx -g "daemon off;"' ${container}
+buildah commit ${container} ${IMAGE_NAME}
+buildah tag ${IMAGE_NAME} ${IMAGE_COMMIT}
+buildah delete ${container}
