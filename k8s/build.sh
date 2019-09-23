e5d1b540796f06d191db75704d182da5eb7f3af6
James Addison <james@reciperadar.com>
2019-09-23 11:04:25 -0700

    Fix image tagging during build

--- k8s/build.sh
+++ k8s/build.sh
@@ -4,5 +4,4 @@ IMAGE_COMMIT=$(git rev-parse --short HEAD)
 container=$(buildah from docker.io/library/nginx:latest)
 buildah copy ${container} 'html' '/usr/share/nginx/html'
 buildah config --port 80 --entrypoint '/usr/sbin/nginx -g "daemon off;"' ${container}
-buildah commit --squash --rm ${container} ${IMAGE_NAME}
-buildah tag ${IMAGE_NAME} ${IMAGE_COMMIT}
+buildah commit --squash --rm ${container} ${IMAGE_NAME}:${IMAGE_COMMIT}
