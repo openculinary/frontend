95c163b276b6931abe593b1334952f95657a8984
James Addison <james@reciperadar.com>
2019-10-04 09:46:41 -0700

    Use alpine-based images

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ if [ -n "${GITLAB_USER_ID}" ]; then
     sed -i '/^mountopt =.*/d' /etc/containers/storage.conf
 fi
 
-container=$(buildah from docker.io/library/nginx:latest)
+container=$(buildah from docker.io/library/nginx:alpine)
 buildah copy ${container} 'html' '/usr/share/nginx/html'
 buildah config --port 80 --entrypoint '/usr/sbin/nginx -g "daemon off;"' ${container}
 buildah commit --squash --rm ${container} ${IMAGE_NAME}:${IMAGE_COMMIT}
