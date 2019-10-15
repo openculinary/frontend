607fe64a6b86407be49d0585fca3cdda42efb4fa
James Addison <james@reciperadar.com>
2019-10-15 11:19:17 +0000

    User interface updates

--- html/js/search.js
+++ html/js/search.js
@@ -26,44 +26,83 @@ function renderUnits(token) {
   return renderText(token);
 }
 
-function contentFormatter(value, row, index) {
-  var content = $('<div />');
-  $('<img />', {'src': 'images/domains/' + row.domain + '.ico', 'alt': ''}).appendTo(content);
+function titleFormatter(recipe) {
+  var title = $('<div />', {'class': 'title'});
+  $('<img />', {'src': 'images/domains/' + recipe.domain + '.ico', 'alt':''}).appendTo(title);
   $('<a />', {
-    'href': row.src,
-    'text': row.title,
+    'href': recipe.src,
+    'text': recipe.title,
     'target': '_blank',
     'rel': 'noreferrer'
-  }).appendTo(content);
-  var ingredients = $('<ul />', {'class': 'ingredients'}).appendTo(content);
-  $.each(row.ingredients, function() {
-    $('<li />', {'html': this.tokens.map(renderToken).join('')}).appendTo(ingredients);
-  });
-  var directions = $('<ul />', {'class': 'directions collapse'}).appendTo(content);
-  $.each(row.directions, function() {
-    $('<li />', {'html': this.tokens.map(renderToken).join('')}).appendTo(directions);
-  });
-  return content.html();
+  }).appendTo(title);
+  return title;
 }
 
-function metadataFormatter(value, row, index) {
-  var duration = moment.duration(row.time, 'minutes');
-  var metadata = $('<div />');
-  $('<img />', {'src': row.image_url, 'alt': row.title}).appendTo(metadata);
-  $('<span />', {'html': '<strong>serves</strong>'}).appendTo(metadata);
-  $('<span />', {'text': row.servings}).appendTo(metadata);
-  $('<br />').appendTo(metadata);
-  $('<span />', {'html': '<strong>time</strong>'}).appendTo(metadata);
-  $('<span />', {'text': duration.as('minutes') + ' mins'}).appendTo(metadata);
-  $('<button />', {
-    'class': 'btn btn-outline-primary toggle-directions',
-    'text': 'View instructions'
-  }).appendTo(metadata);
-  $('<button />', {
+function sidebarFormatter(recipe) {
+  var duration = moment.duration(recipe.time, 'minutes');
+  var sidebar = $('<td />', {'class': 'sidebar'});
+  $('<img />', {'src': recipe.image_url, 'alt': recipe.title}).appendTo(sidebar);
+  $('<span />', {'html': '<strong>serves</strong>'}).appendTo(sidebar);
+  $('<span />', {'text': recipe.servings}).appendTo(sidebar);
+  $('<br />').appendTo(sidebar);
+  $('<span />', {'html': '<strong>time</strong>'}).appendTo(sidebar);
+  $('<span />', {'text': duration.as('minutes') + ' mins'}).appendTo(sidebar);
+  return sidebar;
+}
+
+function contentFormatter(recipe) {
+  var content = $('<td />', {'class': 'content'});
+
+  var tabs = $('<div />', {'class': 'nav tabs'});
+  tabs.append($('<a />', {
+    'class': 'nav-link active',
+    'text': 'Ingredients',
+    'data-target': 'ingredients'
+  }));
+  tabs.append($('<a />', {
+    'class': 'nav-link',
+    'text': 'Directions',
+    'data-target': 'directions'
+  }));
+  content.append(tabs);
+
+  var ingredients = $('<div />', {'class': 'tab ingredients'});
+  var ingredientList = $('<ul />');
+  $.each(recipe.ingredients, function() {
+    ingredientList.append($('<li />', {'html': this.tokens.map(renderToken).join('')}));
+  });
+  ingredients.append(ingredientList);
+  ingredients.append($('<button />', {
     'class': 'btn btn-outline-primary add-to-shopping-list',
     'text': 'Add to shopping list'
-  }).appendTo(metadata);
-  return metadata.html();
+  }));
+  content.append(ingredients);
+
+  var directions = $('<div />', {'class': 'tab directions collapse'});
+  var directionList = $('<ul />');
+  $.each(recipe.directions, function() {
+    directionList.append($('<li />', {'html': this.tokens.map(renderToken).join('')}));
+  });
+  directions.append(directionList);
+  content.append(directions);
+
+  return content;
+}
+
+function recipeFormatter(value, recipe, index) {
+  var container = $('<div />');
+  var title = titleFormatter(recipe);
+  var table = $('<table />', {
+    'class': 'tablesaw tablesaw-stack',
+    'data-tablesaw-mode': 'stack'
+  });
+  var row = $('<tr />');
+  row.append(sidebarFormatter(recipe));
+  row.append(contentFormatter(recipe));
+  row.appendTo(table);
+  container.append(title);
+  container.append(table);
+  return container.html();
 }
 
 function getRecipeProducts(recipe) {
@@ -98,11 +137,13 @@ function rowAttributes(row) {
   }
 }
 
-function toggleDirections() {
+function selectTab() {
   var recipe = getRecipe(this);
-  $(`#search .results .recipe[data-id="${recipe.id}"] ul.ingredients`).toggleClass('collapse');
-  $(`#search .results .recipe[data-id="${recipe.id}"] ul.directions`).toggleClass('collapse');
-  $(this).text($(this).text() === 'View instructions' ? 'View ingredients' : 'View instructions');
+  var target = $(this).data('target');
+  $(`#search .results .recipe[data-id="${recipe.id}"] div.tabs a`).removeClass('active');
+  $(`#search .results .recipe[data-id="${recipe.id}"] div.tabs a[data-target="${target}"]`).addClass('active');
+  $(`#search .results .recipe[data-id="${recipe.id}"] div.tab`).addClass('collapse');
+  $(`#search .results .recipe[data-id="${recipe.id}"] div.tab.${target}`).removeClass('collapse');
 }
 
 function recipeRedirect() {
@@ -197,9 +238,9 @@ $('#search .results table').on('load-success.bs.table', function() {
   var paginationDetail = $('#search .results div.pagination-detail').empty();
   if ($.bbq.getState('action') === 'search') sortPrompt.appendTo(paginationDetail);
 
-  $(this).find('.metadata button.add-to-shopping-list').on('click', addRecipe);
-  $(this).find('.metadata button.toggle-directions').on('click', toggleDirections);
-  $(this).find('.recipe a').on('click', recipeRedirect);
+  $(this).find('.title a').on('click', recipeRedirect);
+  $(this).find('.content .tabs a.nav-link').on('click', selectTab);
+  $(this).find('.content button.add-to-shopping-list').on('click', addRecipe);
 });
 
 $('#search .results table').on('post-body.bs.table', function(data) {
