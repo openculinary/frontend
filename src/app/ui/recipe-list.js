cfd6f9454934974956b60a82e45c635694eb1e36
James Addison <james@reciperadar.com>
2019-11-27 17:30:52 +0000

    Preparatory import

--- src/app/ui/recipe-list.js
+++ src/app/ui/recipe-list.js
@@ -1,6 +1,7 @@
 import 'jquery';
 import * as moment from 'moment';
 import 'tablesaw/dist/stackonly/tablesaw.stackonly.jquery.js';
+import 'convert-units';
 
 import 'tablesaw/dist/stackonly/tablesaw.stackonly.css';
 import './recipe-list.css'
