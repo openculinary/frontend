019ceddeb514b780215bd4fa37848e070dac4c78
James Addison <james@reciperadar.com>
2019-11-29 14:01:01 +0000

    Initialize countly

--- src/countly/index.js
+++ src/countly/index.js
@@ -1,1 +1,17 @@
-import { Countly } from 'countly-sdk-web';
+import Countly from 'countly-sdk-web';
+
+var hostname = window.location.hostname;
+var hostURI = window.location.href.split('#')[0];
+
+var countlyHostname = `countly.${hostname}`;
+var countlyURI = hostURI.replace(hostname, countlyHostname);
+
+Countly.init({
+  app_key: 'tbd',
+  force_post: true,
+  url: countlyURI,
+  use_session_cookie: false
+});
+Countly.track_sessions()
+Countly.track_pageview()
+Countly.track_errors()
