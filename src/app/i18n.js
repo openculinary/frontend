a1391868a755c5eee6b57189d39d8f4cdc17e519
James Addison <james@reciperadar.com>
2019-12-17 11:10:03 +0000

    Add language detection

--- src/app/i18n.js
+++ src/app/i18n.js
@@ -3,6 +3,7 @@ import jqueryi18next from 'jquery-i18next';
 
 import i18next from 'i18next';
 import XHR from 'i18next-xhr-backend';
+import BrowserLanguage from 'i18next-browser-languagedetector';
 
 export { localize };
 
@@ -18,9 +19,13 @@ function localize(selector) {
   $(selector).localize();
 }
 
-i18next.use(XHR).init({
+i18next.use(BrowserLanguage).use(XHR).init({
   backend: {
     loadPath: 'i18n/{{lng}}/{{ns}}.json'
+  },
+  detection: {
+    caches: ['localStorage'],
+    order: ['localStorage', 'navigator', 'htmlTag']
   }
 }, function() {
   jqueryi18next.init(i18next, $);
