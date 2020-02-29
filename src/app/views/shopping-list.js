11d8ff536b5ef9a6916e01d719b56579a8b78cd8
James Addison <james@reciperadar.com>
2020-02-29 17:35:09 +0000

    Set namespace on data-i18n attributes

--- src/app/views/shopping-list.js
+++ src/app/views/shopping-list.js
@@ -28,7 +28,7 @@ function renderProductText(product, mealCounts) {
 
 function categoryElement(category) {
   var fieldset = $('<fieldset />', {'class': category});
-  $('<legend />', {'data-i18n': `categories.${category}`}).appendTo(fieldset);
+  $('<legend />', {'data-i18n': `categories:${category}`}).appendTo(fieldset);
   return fieldset;
 }
 
