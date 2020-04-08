cb5e5b0ecd744364c743a4247b1b63a7f1eb61cb
James Addison <james@reciperadar.com>
2020-04-08 11:21:32 +0100

    Remove prototype development environment configuration

--- develop.sh
+++
@@ -1,9 +0,0 @@
-HOSTNAME=$(hostname)
-REPO_PATH=$(git rev-parse --show-toplevel)
-
-kubectl delete -f k8s/development
-cat k8s/development/*.yml \
-  | sed -e "s|HOSTNAME|${HOSTNAME}|g" \
-  | sed -e "s|REPO_PATH|${REPO_PATH}|g" \
-  | kubectl create -f -
-kubectl patch deployment frontend-deployment --patch "$(cat k8s/development/web-deployment.yml.patch)"
