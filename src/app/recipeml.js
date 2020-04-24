e923833f519bbb750b8d2e40fb2208e5de456618
James Addison <james@reciperadar.com>
2020-04-24 17:28:09 +0100

    Invoke xslt-ts transformation

--- src/app/recipeml.js
+++ src/app/recipeml.js
@@ -1,5 +1,26 @@
+import '@ungap/global-this';
+import { DOMImplementationImpl, DOMParserImpl, XMLSerializerImpl } from 'xmldom-ts';
+import { install, xsltProcess } from 'xslt-ts';
+
 export { renderToHTML };
 
-function renderToHTML(recipeML) {
-    return '<div />';
+const template = `
+<?xml version="1.0" encoding="utf-8" ?>
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+<xsl:template match="/"><div /></xsl:template>
+</xsl:stylesheet>
+`;
+
+
+function renderToHTML(doc) {
+    const parser = new DOMParserImpl();
+    const serializer = new XMLSerializerImpl();
+    const dom = new DOMImplementationImpl();
+
+    install(parser, serializer, dom);
+
+    const recipeML = parser.parseFromString(doc, 'text/xml');
+    const recipeXSLT = parser.parseFromString(template, 'text/xml');
+
+    return xsltProcess(recipeML, recipeXSLT);
 }
