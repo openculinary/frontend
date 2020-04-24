c4883d033429c11d5cc0227387d1cbdacd2cb1bc
James Addison <james@reciperadar.com>
2020-04-24 18:43:27 +0100

    Implement simple ingredient rendering case

--- src/app/recipeml.js
+++ src/app/recipeml.js
@@ -1,3 +1,4 @@
+import 'array-flat-polyfill';
 import '@ungap/global-this';
 import { DOMImplementationImpl, DOMParserImpl, XMLSerializerImpl } from 'xmldom-ts';
 import { install, xsltProcess } from 'xslt-ts';
@@ -7,7 +8,23 @@ export { renderToHTML };
 const template = `
 <?xml version="1.0" encoding="utf-8" ?>
 <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
-<xsl:template match="/"><div /></xsl:template>
+
+<xsl:template match="/">
+  <xsl:apply-templates />
+</xsl:template>
+
+<xsl:template match="amt">
+<div class="quantity">
+  <xsl:value-of select="qty/text()" />
+  <xsl:text> </xsl:text>
+  <xsl:value-of select="unit/text()" />
+</div>
+</xsl:template>
+
+<xsl:template match="ingredient">
+<div class="product"><xsl:value-of select="text()" /></div>
+</xsl:template>
+
 </xsl:stylesheet>
 `;
 
