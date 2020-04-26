a60e032e8241f2a8a8fd20ea28cb61fb7d292656
James Addison <james@reciperadar.com>
2020-04-26 16:08:24 +0100

    Switch to fork of html2canvas including icon-rendering fix

--- src/feedback/loader.js
+++ src/feedback/loader.js
@@ -1,11 +1,12 @@
 import 'jquery';
+import 'html2canvas/dist/html2canvas.esm.js';
 
 import { Feedback } from './feedback';
 import './feedback.css';
 
 $(function() {
   Feedback({
-    h2cPath: 'vendors/html2canvas/html2canvas.min.js',
+    h2cPath: 'html2canvas.js',
     url: 'api/feedback',
     scrollX: 0,
     scrollY: 0,
