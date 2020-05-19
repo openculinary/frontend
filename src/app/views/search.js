c0ea03f76052a8fb42ba488bd45eccb774155e5f
James Addison <james@reciperadar.com>
2020-05-19 09:11:03 -0700

    Refactor jquery attribute assignment

--- src/app/views/search.js
+++ src/app/views/search.js
@@ -110,10 +110,10 @@ function createSortPrompt() {
 
   var sortSelect = $('<select>', {'class': 'sort'}).attr('aria-label', 'Recipe sort selection');
   $(sortOptions).each(function() {
-    var sortOption = $('<option>');
-    sortOption.text(this.text);
-    sortOption.attr('data-i18n', i18nAttr(`search:sort-${this.val}`));
-    sortOption.attr('value', this.val);
+    var sortOption = $('<option>', {
+      'data-i18n': i18nAttr(`search:sort-${this.val}`),
+      'value': this.val
+    });
     if (sortChoice === this.val) sortOption.attr('selected', 'selected');
     sortSelect.append(sortOption);
   });
@@ -129,7 +129,7 @@ function createSortPrompt() {
     $(window).trigger('popstate');
   });
 
-  var sortPrompt = $('<span>').text('Order by ');
+  var sortPrompt = $('<span>', {'data-i18n': i18nAttr('search:sort-selection-prompt')});
   sortSelect.appendTo(sortPrompt);
 
   return sortPrompt;
