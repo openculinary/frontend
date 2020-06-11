2e9b28ccd1ce29cbf25a2a84b498b49de8af6b27
James Addison <james@reciperadar.com>
2020-06-11 19:08:08 +0100

    Render recipe details

--- src/app/views/recipe.js
+++ src/app/views/recipe.js
@@ -1,6 +1,7 @@
 import './recipe.css';
 
 import { getRecipeById } from '../common';
+import { renderIngredientHTML, renderDirectionHTML } from '../recipeml';
 import { getState, loadPage } from '../state';
 
 export { renderRecipe };
@@ -9,7 +10,24 @@ function renderRecipe() {
   var id = getState().id;
   var recipe = getRecipeById(id);
 
-  $('#recipe div.title').text(recipe.title);
+  var title = $('#recipe div.title').empty();
+  var image = $('#recipe div.image').empty();
+  var ingredients = $('#recipe div.ingredients').empty();
+  var directions = $('#recipe div.directions').empty();
+
+  var link = $('<a />', {'href': recipe.dst});
+  $('<img />', {'src': recipe.image_url, 'alt': recipe.title}).appendTo(link);
+
+  title.text(recipe.title);
+  image.append(link);
+  $.each(recipe.ingredients, function() {
+    ingredients.append(renderIngredientHTML(this.markup, this.product.state));
+    ingredients.append($('<div  />', {'style': 'clear: both'}));
+  });
+  $.each(recipe.directions, function() {
+    directions.append(renderDirectionHTML(this.markup));
+    directions.append($('<div  />', {'style': 'clear: both'}));
+  });
 
   loadPage('recipe');
 }
