9e432101bd89a6fd3a3970ded01dedf666ef8418
James Addison <james@reciperadar.com>
2020-06-17 13:33:22 +0100

    Move product existence check to after product-field-completion

--- src/app/models/products.js
+++ src/app/models/products.js
@@ -22,16 +22,15 @@ function aggregateUnitQuantities(product, mealCounts) {
 
 function addProduct(product, recipeId) {
   var products = storage.products.load();
-  if (product.product_id in products) {
-    product.category = products[product.product_id].category;
-  }
   if (!product.state) {
     product.state = 'required';
   }
   if (!product.recipes) {
     product.recipes = {};
   }
-
+  if (product.product_id in products) {
+    product.category = products[product.product_id].category;
+  }
   if (recipeId) {
     if (!(recipeId in product.recipes)) {
       product.recipes[recipeId] = {amounts: []};
