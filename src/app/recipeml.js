84c74293c045165f320f45c8c65924995506108f
James Addison <james@reciperadar.com>
2020-06-17 18:55:35 +0100

    Nit: remove unnecessary backticks

--- src/app/recipeml.js
+++ src/app/recipeml.js
@@ -19,7 +19,7 @@ function renderIngredientHTML(ingredient) {
     container.append($('<div />', {'class': 'quantity', 'html': quantityHTML}));
 
     $(xml.childNodes).remove('amt');
-    $(xml).find('ingredient').replaceWith((idx, text) => $('<span />', {'class': `tag badge`, 'text': text}).addClass(ingredient.product && ingredient.product.state));
+    $(xml).find('ingredient').replaceWith((idx, text) => $('<span />', {'class': 'tag badge', 'text': text}).addClass(ingredient.product && ingredient.product.state));
     container.append($('<div />', {'class': 'product', 'html': xml.childNodes}));
 
     return container.html();
