72758eeec6d5d5f9ab1bc82ed8b9c4bde30113a2
James Addison <james@reciperadar.com>
2020-06-18 18:37:27 +0100

    Delete incorrect 'remove' class from recipe link on meal planner

--- src/app/views/meals.js
+++ src/app/views/meals.js
@@ -65,7 +65,7 @@ function recipeElement(recipe) {
   // TODO: only include 'servings' parameter when the value overrides the recipe default
   // This may require some data model refactoring
   var link = $('<a />', {
-    'class': 'remove fa fa-link',
+    'class': 'fa fa-link',
     'href': `#search&action=view&id=${recipe.id}&servings=${recipe.servings}`
   });
   var item = $('<div />', {'style': 'float: left'});
