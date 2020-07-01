dc9df4836299ac36fb17ecad745b4cbe53d5de7a
James Addison <james@reciperadar.com>
2020-07-01 16:07:34 +0100

    Remove legacy product storage paths

--- src/app/models/products.js
+++ src/app/models/products.js
@@ -1,28 +1,11 @@
 import 'jquery';
 
 import { db } from '../database';
-import { storage } from '../storage';
 
 export { addProduct, removeProduct };
 
 function addProduct(ingredient, recipeId, index) {
   var product = ingredient.product;
-  product.state = product.state || 'required';
-  product.recipes = product.recipes || {};
-
-  var products = storage.products.load();
-  if (product.product_id in products) {
-    product.category = products[product.product_id].category;
-    Object.assign(product.recipes, products[product.product_id].recipes);
-  }
-
-  if (recipeId) {
-    product.recipes[recipeId] = product.recipes[recipeId] || {quantities: []};
-    product.recipes[recipeId].quantities.push(ingredient.quantity);
-  }
-
-  storage.products.remove({'hashCode': product.product_id});
-  storage.products.add({'hashCode': product.product_id, 'value': product});
   db.products.put({
     id: product.product_id,
     category: product.category,
@@ -42,13 +25,8 @@ function removeProduct(product, recipeId) {
   db.ingredients
     .where("[recipe_id+product_id+index]")
     .between(
-      [recipeId || '', product.product_id, db.minKey()],
-      [recipeId || '', product.product_id, db.maxKey()]
+      [recipeId || '', product.id, db.minKey()],
+      [recipeId || '', product.id, db.maxKey()]
     )
     .delete();
-
-  if (recipeId) delete product.recipes[recipeId];
-  if (Object.keys(product.recipes).length) return;
-
-  storage.products.remove({'hashCode': product.product_id});
 }
