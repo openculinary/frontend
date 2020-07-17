2263c513aa66868b29edad59f2c51459a3f9d398
James Addison <james@reciperadar.com>
2020-07-17 19:02:42 +0100

    Remove redundant CSS file

--- src/diagnostics/main.js
+++ src/diagnostics/main.js
@@ -1,8 +1,6 @@
 import $ from 'jquery';
 import { create as jsondiffpatch, formatters as diffformatters } from 'jsondiffpatch';
-
 import 'jsondiffpatch/dist/formatters-styles/html.css'
-import './style.css';
 
 function getRecipeId() {
   var urlParams = new URLSearchParams(window.location.hash.slice(1));
