46b371f3e2787b2f36aefe71061f11f85a19296e
James Addison <james@reciperadar.com>
2020-07-17 17:28:28 +0100

    Run jsondiffpatch over recipe diagnostic data

--- src/diagnostics/main.js
+++ src/diagnostics/main.js
@@ -1,4 +1,5 @@
 import $ from 'jquery';
+import { create as jsondiffpatch } from 'jsondiffpatch';
 
 function getRecipeId() {
   var urlParams = new URLSearchParams(window.location.hash.slice(1));
@@ -6,5 +7,9 @@ function getRecipeId() {
 }
 
 $(function() {
-  console.log(getRecipeId());
+  var recipeId = getRecipeId();
+  $.ajax({url: `/diagnostics/recipes/${recipeId}`}).then(recipe => {
+    var diff = jsondiffpatch().diff(recipe.indexed, recipe.crawled);
+    console.log(diff);
+  });
 });
