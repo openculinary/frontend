46a1f93392e746980567d59a3b304cda3a02bf1d
James Addison <james@reciperadar.com>
2020-07-27 14:54:58 +0100

    Use self.skipWaiting in preference to workbox-core.skipWaiting (h/t @zdila)

--- src/sw/sw.js
+++ src/sw/sw.js
@@ -1,11 +1,10 @@
-import { skipWaiting } from 'workbox-core';
 import { precacheAndRoute } from 'workbox-precaching';
 import { registerRoute } from 'workbox-routing';
 import { NetworkFirst } from 'workbox-strategies';
 
 addEventListener('message', (event) => {
   if (event.data && event.data.type == 'skipWaiting') {
-    skipWaiting();
+    self.skipWaiting();
   }
 });
 
