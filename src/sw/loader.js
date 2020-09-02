8ee09b48de1b1b5f3651f7c43ce401d3fa438092
James Addison <james@reciperadar.com>
2020-09-02 09:59:18 +0100

    Switch to use de-facto standard 'SKIP_WAITING' message type from service-worker loader

--- src/sw/loader.js
+++ src/sw/loader.js
@@ -11,7 +11,7 @@ $(function() {
         wb.addEventListener('controlling', () => {
           window.location.reload();
         });
-        wb.messageSW({type: 'skipWaiting'});
+        wb.messageSW({type: 'SKIP_WAITING'});
       }
     });
     wb.register();
