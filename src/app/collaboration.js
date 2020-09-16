3eb4d35f8c87f8e4f7eacf94fba71783ef9acf29
James Addison <james@reciperadar.com>
2020-09-16 19:09:26 +0100

    Add indexeddb persistence

--- src/app/collaboration.js
+++ src/app/collaboration.js
@@ -1,10 +1,15 @@
 import * as Y from 'yjs';
 import { WebsocketProvider } from 'y-websocket';
+import { IndexeddbPersistence } from 'y-indexeddb';
 
 const doc = new Y.Doc();
 const sessionId = 'example-session-id';
 const wsProvider = new WebsocketProvider('ws://localhost/collaboration', sessionId, doc);
+const dbProvider = new IndexeddbPersistence(sessionId, doc);
 
 wsProvider.on('status', event => {
   console.log(event.status);
+})
+dbProvider.on('synced', () => {
+  console.log('y-indexeddb synced');
 })
