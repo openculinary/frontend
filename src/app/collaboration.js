02784b415432778ed6939592c63b7fb23d9fed61
James Addison <james@reciperadar.com>
2020-09-16 19:09:15 +0100

    Extract sessionId variable

--- src/app/collaboration.js
+++ src/app/collaboration.js
@@ -2,7 +2,8 @@ import * as Y from 'yjs';
 import { WebsocketProvider } from 'y-websocket';
 
 const doc = new Y.Doc();
-const wsProvider = new WebsocketProvider('ws://localhost/collaboration', 'my-roomname', doc);
+const sessionId = 'example-session-id';
+const wsProvider = new WebsocketProvider('ws://localhost/collaboration', sessionId, doc);
 
 wsProvider.on('status', event => {
   console.log(event.status);
