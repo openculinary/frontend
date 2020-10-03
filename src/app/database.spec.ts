d00543cc9c0b3dd79a4b41fe77db29864672536b
James Addison <james@reciperadar.com>
2020-10-04 00:18:48 +0100

    Add simple test case assertion

--- src/app/database.spec.ts
+++ src/app/database.spec.ts
@@ -1,10 +1,12 @@
 import * as assert from 'assert';
 
 import { db } from './database';
-import { packageVersion } from 'document';
+import { packageVersion, types } from 'document';
 
 describe('test', () => {
     it('load database state from document', async () => {
-        await db.starred.count();
+        db.starred.add(new types.Starred('example', packageVersion));
+        const result = await db.starred.count();
+        assert.equal(result, 1);
     });
 });
