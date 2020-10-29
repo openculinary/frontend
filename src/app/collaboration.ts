ef6942056488312a1c080a0c94b326ecb9ff9003
James Addison <james@reciperadar.com>
2020-10-29 13:42:39 +0000

    Add joinSession collaboration function

--- src/app/collaboration.ts
+++ src/app/collaboration.ts
@@ -2,21 +2,27 @@ import * as Y from 'yjs';
 import { WebsocketProvider } from 'y-websocket';
 import { IndexeddbPersistence } from 'y-indexeddb';
 
-const doc = new Y.Doc();
-const docText = doc.getText('doc.reciperadar.org');
+var doc: Y.Doc;
+var docText: Y.Text;
 
-const sessionId = 'example-session-id';
-const wsProvider = new WebsocketProvider('ws://localhost/collaboration', sessionId, doc);
-const dbProvider = new IndexeddbPersistence(sessionId, doc);
+function joinSession(sessionId: string) {
+  doc = new Y.Doc();
+  const wsProvider = new WebsocketProvider('ws://localhost/collaboration', sessionId, doc);
+  const dbProvider = new IndexeddbPersistence(sessionId, doc);
 
-wsProvider.on('status', event => {
-  console.log(`y-websocket ${event.status}`);
-});
-dbProvider.on('synced', () => {
-  console.log('y-indexeddb synced');
-});
-docText.observe(() => {
-  console.log(`y-doc-text: ${docText.toString()}`);
-});
+  wsProvider.on('status', event => {
+    console.log(`y-websocket ${event.status}`);
+  });
+  dbProvider.on('synced', () => {
+    console.log('y-indexeddb synced');
+    docText = doc.getText('doc.reciperadar.org');
+    docText.observe(() => {
+      console.log(`y-doc-text: ${docText.toString()}`);
+    });
+    docText.insert(0, 'example-text');
+  });
+}
 
-docText.insert(0, 'example-text');
+joinSession('example-session-id');
+
+export { joinSession };
