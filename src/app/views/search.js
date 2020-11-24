738e4bcc065cc381cd482f4d99f5f496493446e5
James Addison <james@reciperadar.com>
2020-11-24 14:14:09 +0000

    Nit: rename domain.id -> domain.key

--- src/app/views/search.js
+++ src/app/views/search.js
@@ -82,14 +82,14 @@ function updateStateDomains() {
 function renderDomainFacet(domain, state) {
   var domainState = state === undefined ? true : state;
   var chip = $('<label />', {'class': 'badge badge-light badge-pill'});
-  var checkbox = $('<input />', {'type': 'checkbox', 'checked': domainState, 'value': domain.id});
-  var icon = $('<img />', {'src': 'images/domains/' + domain.id + '.ico', 'alt':''});
+  var checkbox = $('<input />', {'type': 'checkbox', 'checked': domainState, 'value': domain.key});
+  var icon = $('<img />', {'src': 'images/domains/' + domain.key + '.ico', 'alt':''});
 
   checkbox.on('change', updateStateDomains);
 
   chip.append(checkbox);
   chip.append(icon);
-  chip.append(document.createTextNode(domain.id));
+  chip.append(document.createTextNode(domain.key));
   return chip;
 }
 
@@ -121,10 +121,10 @@ function getDomainStates() {
   var domainStates = {};
   var state = getState();
   if (!state.domains) return domainStates;
-  state.domains.split(',').forEach(domainId => {
-    var excluded = domainId.startsWith('-');
-    domainId = excluded ? domainId.replace('-', '') : domainId;
-    domainStates[domainId] = !excluded;
+  state.domains.split(',').forEach(domainKey => {
+    var excluded = domainKey.startsWith('-');
+    domainKey = excluded ? domainKey.replace('-', '') : domainKey;
+    domainStates[domainKey] = !excluded;
   });
   return domainStates;
 }
@@ -133,7 +133,7 @@ function domainFacetsHandler(data) {
   var domainStates = getDomainStates();
   var domainFacets = $('#search .domain-facets').empty();
   $.each(data.facets.domains, function() {
-    domainFacets.append(renderDomainFacet(this, domainStates[this.id]));
+    domainFacets.append(renderDomainFacet(this, domainStates[this.key]));
   });
   domainFacets.toggleClass('collapse', $.isEmptyObject(data.facets.domains));
 }
