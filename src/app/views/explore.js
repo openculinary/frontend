2112914002f6de9a332b9a908cd310b7136d53d1
James Addison <james@reciperadar.com>
2020-11-30 13:29:05 +0000

    Add explore API call via AJAX

--- src/app/views/explore.js
+++ src/app/views/explore.js
@@ -1,9 +1,24 @@
 import $ from 'jquery';
 import Swipe from 'swipejs';
 
-var current = 'garlic';
+var current;
 var include = [];
 var exclude = [];
+
+function explore() {
+  var params = {include, exclude};
+  var url = '/api/recipes/explore';
+  if (include.length || exclude.length) url += '?' + $.param(params);
+
+  $.ajax({url: url}).then(hits => {
+    console.log(hits);
+  });
+}
+
+function swipeSetup() {
+  explore();
+  current = 'tomato';
+}
 
 function swipeHandler(index, element, direction) {
   var target = (direction > 0) ? include : exclude;
@@ -18,4 +33,5 @@ $(function() {
     draggable: true,
     callback: swipeHandler,
   });
+  swipeSetup();
 });
