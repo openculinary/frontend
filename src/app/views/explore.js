f3896b52bffbcb21a13d770655e0fa3c3731a97a
James Addison <james@reciperadar.com>
2020-11-30 13:17:58 +0000

    Add swipe callback handler

--- src/app/views/explore.js
+++ src/app/views/explore.js
@@ -1,8 +1,21 @@
 import $ from 'jquery';
 import Swipe from 'swipejs';
 
+var current = 'garlic';
+var include = [];
+var exclude = [];
+
+function swipeHandler(index, element, direction) {
+  var target = (direction > 0) ? include : exclude;
+  target.push(current);
+  var params = {include, exclude};
+  console.log(params);
+  current = $(element).text();
+}
+
 $(function() {
   window.explore = new Swipe(document.getElementById('explore-swipe'), {
     draggable: true,
+    callback: swipeHandler,
   });
 });
