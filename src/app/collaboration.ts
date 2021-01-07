6f307c3f5479681a5e3294bde7ee6fb0549f741f
James Addison <james@reciperadar.com>
2021-01-07 16:48:36 +0000

    Provide the ability to connect and disconnect from collaborative sessions

--- src/app/collaboration.ts
+++ src/app/collaboration.ts
@@ -14,9 +14,9 @@ function bindShoppingList(shoppingListNotes, awareness) {
 }
 
 function renderPeers(peerStates) {
-  const collaboration = $('#collaboration').empty();
+  const peers = $('#collaboration peers').empty();
   peerStates.forEach((state, clientId) => {
-    collaboration.append($('<div />', {
+    peers.append($('<div />', {
       'data-client-id': clientId,
       'text': clientId,
     }));
@@ -27,20 +27,59 @@ function handleAwarenessUpdates(awareness) {
   awareness.on('change', () => renderPeers(awareness.getStates()));
 }
 
+function displayConnectionStatus(wsProvider) {
+  // TODO: i18n
+  const disconnect = $('<button />', {'text': 'Disconnect'});
+  disconnect.on('click', () => wsProvider.disconnect());
+
+  const connection = $('#collaboration .connection').empty();
+  connection.append($('<div />', {'text': `Connected to ${wsProvider.roomname}`}));
+  connection.append(disconnect);
+}
+
+function clearConnectionStatus() {
+  // TODO: i18n
+  const connect = $('<button />', {'text': 'Connect'});
+  connect.on('click', () => {
+    const sessionId = $('#collaboration .connection input.session-id').val();
+    joinSession(sessionId);
+  });
+
+  const connection = $('#collaboration .connection').empty();
+  connection.append($('<input />', {'class': 'session-id', 'type': 'text', 'placeholder': 'example-session-id'}));
+  connection.append(connect);
+}
+
 function joinSession(sessionId: string) {
   const doc = new Y.Doc();
   const shoppingListNotes = doc.getText('doc.reciperadar.com/v1/shopping-list/notes');
 
-  const awareness = new Awareness(doc);
   const dbProvider = new IndexeddbPersistence(sessionId, doc);
-  const wsProvider = new WebsocketProvider(`${window.location.protocol === 'https:' ? 'wss': 'ws'}://${window.location.host}/collaboration`, sessionId, doc, { awareness });
-
   dbProvider.on('synced', () => {
+    const awareness = new Awareness(doc);
     bindShoppingList(shoppingListNotes, awareness);
     handleAwarenessUpdates(awareness);
+
+    // Once the local state is synced, open a websocket connection
+    const wsProvider = new WebsocketProvider(`${window.location.protocol === 'https:' ? 'wss': 'ws'}://${window.location.host}/collaboration`, sessionId, doc, { awareness });
+
+    // Configure connection event handling
+    wsProvider.on('status', event => {
+      switch (event.status) {
+        case 'connected':
+          displayConnectionStatus(wsProvider);
+          break;
+        case 'disconnected':
+          dbProvider.destroy();
+          awareness.destroy();
+          wsProvider.destroy();
+          clearConnectionStatus();
+          break;
+      }
+    });
   });
 }
 
-joinSession('example-session-id');
-
-export { joinSession };
+$(function() {
+  clearConnectionStatus();
+})
