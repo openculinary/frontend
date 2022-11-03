1ddac44dedd69a34e4adc7375230e0fa0b96933b
James Addison <james@reciperadar.com>
2022-11-03 17:55:42 +0000

    Fixup: update test expectations
    Test cases were not updated alongside 5a33570e7c856c07f48e6c390d144ba540ca047b

--- test/recipeml.js
+++ test/recipeml.js
@@ -23,7 +23,7 @@ describe('html rendering', function() {
 
   it('renders simple product', function() {
     var recipeML = '<amt><qty>0.5</qty><unit>bag</unit></amt><ingredient>potato wedges</ingredient>';
-    var expected = '<div class="quantity"><sup>1</sup>⁄<sub>2</sub> bag</div><div class="product"><span class="tag badge">potato wedges</span></div>';
+    var expected = '<div class="quantity"><sup>1</sup>⁄<sub>2</sub> bag</div><div class="ingredient"><span class="product">potato wedges</span></div>';
 
     var ingredient = ingredientHelper(recipeML, 0.5, 'bag');
     var rendered = renderIngredientHTML(ingredient);
@@ -33,7 +33,7 @@ describe('html rendering', function() {
 
   it('renders contextual product', function() {
     var recipeML = '<amt><qty>1</qty><unit>whole</unit></amt>small <ingredient>onion</ingredient> diced';
-    var expected = '<div class="quantity">1 whole</div><div class="product">small <span class="tag badge">onion</span> diced</div>';
+    var expected = '<div class="quantity">1 whole</div><div class="ingredient">small <span class="product">onion</span> diced</div>';
 
     var ingredient = ingredientHelper(recipeML, 1, 'whole');
     var rendered = renderIngredientHTML(ingredient);
@@ -43,7 +43,7 @@ describe('html rendering', function() {
 
   it('renders without units', function() {
     var recipeML = '<amt><qty>1</qty></amt><ingredient>onion</ingredient>';
-    var expected = '<div class="quantity">1</div><div class="product"><span class="tag badge">onion</span></div>';
+    var expected = '<div class="quantity">1</div><div class="ingredient"><span class="product">onion</span></div>';
 
     var ingredient = ingredientHelper(recipeML, 1);
     var rendered = renderIngredientHTML(ingredient);
@@ -53,7 +53,7 @@ describe('html rendering', function() {
 
   it('renders human quantities', function() {
     var recipeML = '<amt><qty>14.79</qty><unit>ml</unit></amt><ingredient>olive oil</ingredient>';
-    var expected = '<div class="quantity">3 teaspoons</div><div class="product"><span class="tag badge">olive oil</span></div>';
+    var expected = '<div class="quantity">3 teaspoons</div><div class="ingredient"><span class="product">olive oil</span></div>';
 
     var ingredient = ingredientHelper(recipeML, 14.79, 'ml');
     var rendered = renderIngredientHTML(ingredient);
@@ -63,7 +63,7 @@ describe('html rendering', function() {
 
   it('always renders a quantity element', function() {
     var recipeML = '<ingredient>bananas</ingredient>';
-    var expected = '<div class="quantity"></div><div class="product"><span class="tag badge">bananas</span></div>';
+    var expected = '<div class="quantity"></div><div class="ingredient"><span class="product">bananas</span></div>';
 
     var ingredient = ingredientHelper(recipeML);
     var rendered = renderIngredientHTML(ingredient);
@@ -73,7 +73,7 @@ describe('html rendering', function() {
 
   it('renders ingredient state', function() {
     var recipeML = '<ingredient>garlic</ingredient>';
-    var expected = '<div class="quantity"></div><div class="product"><span class="tag badge available">garlic</span></div>';
+    var expected = '<div class="quantity"></div><div class="ingredient"><span class="product available">garlic</span></div>';
 
     var ingredient = ingredientHelper(recipeML, null, null, 'available');
     var rendered = renderIngredientHTML(ingredient);
@@ -83,7 +83,7 @@ describe('html rendering', function() {
 
   it('renders simple direction', function() {
     var recipeML = 'place the <mark class="equipment vessel">casserole dish</mark> in the <mark class="equipment appliance">oven</mark>';
-    var expected = '<li class="direction">place the <span class="equipment tag badge">casserole dish</span> in the <span class="equipment tag badge">oven</span></li>';
+    var expected = '<li class="direction">place the <span class="equipment">casserole dish</span> in the <span class="equipment">oven</span></li>';
 
     var direction = directionHelper(recipeML);
     var rendered = renderDirectionHTML(direction);
@@ -93,7 +93,7 @@ describe('html rendering', function() {
 
   it('renders directions with actions and equipment', function() {
     var recipeML = '<mark class="action">heat</mark> the <mark class="equipment appliance">oven</mark> to 200 F';
-    var expected = '<li class="direction"><span class="action">heat</span> the <span class="equipment tag badge">oven</span> to 200 F</li>';
+    var expected = '<li class="direction"><span class="action">heat</span> the <span class="equipment">oven</span> to 200 F</li>';
 
     var direction = directionHelper(recipeML);
     var rendered = renderDirectionHTML(direction);
