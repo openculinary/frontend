7480d008de179469ae6cd6bde47f6733de282b7c
James Addison <james@reciperadar.com>
2022-11-03 22:07:22 +0000

    Attempt to fix state-hash comparison condition; requires further testing

--- src/app/views/explore.ts
+++ src/app/views/explore.ts
@@ -13,7 +13,7 @@ function pushExplore() {
   // If the requested search is a repeat of the current state, perform a results refresh
   // This is done to ensure that the results are scrolled into view
   const stateHash: string = renderStateHash(state);
-  if (`#${window.location.hash}` === stateHash) {
+  if (window.location.hash === stateHash) {
     $('#explore table[data-row-attributes]').trigger('page-change.bs.table');
   }
   pushState(state, stateHash);
