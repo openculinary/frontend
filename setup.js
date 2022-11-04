df1fc76c7837660ae913b9186f1e9b74969548e5
James Addison <james@reciperadar.com>
2022-11-04 18:32:24 +0000

    Provide 'location' global during unit testing

--- setup.js
+++ setup.js
@@ -3,6 +3,7 @@ const dom = new JSDOM('<html />');
 
 global.document = dom.window.document;
 global.history = dom.window.history;
+global.location = dom.window.location;
 global.navigator = dom.window.navigator;
 global.window = dom.window;
 global.self = dom.window;
