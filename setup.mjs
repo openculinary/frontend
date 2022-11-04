9eefbcd6ed313eaef52fae7ae89398ce9c9e4657
James Addison <james@reciperadar.com>
2022-11-04 19:04:01 +0000

    Fixup: provide 'Element' global during unit testing

--- setup.mjs
+++ setup.mjs
@@ -1,6 +1,7 @@
 import { JSDOM } from 'jsdom';
 const dom = await JSDOM.fromFile('public/index.html');
 
+global.Element = dom.window.Element;
 global.document = dom.window.document;
 global.history = dom.window.history;
 global.location = dom.window.location;
