49f263303a8313a74eb21da179c98c4a040a6472
James Addison <james@reciperadar.com>
2022-11-04 18:28:57 +0000

    Fixup: mocha unit test framework for state testing

--- test/state.js
+++ test/state.js
@@ -2,10 +2,14 @@ import * as assert from 'assert';
 
 import { pushState } from '../src/app/state';
 
-describe('duplicate state attempts', function() {
-  assert.equal(1, window.history.length);
-  pushState({}, '#example');
-  window.history.go();
-  pushState({}, '#example');
-  assert.equal(2, window.history.length);
+describe('state navigation', function() {
+
+  it('does not add duplicate entries', function () {
+    assert.equal(1, window.history.length);
+    pushState({}, '#example');
+    window.history.go();
+    pushState({}, '#example');
+    assert.equal(2, window.history.length);
+  });
+
 })
