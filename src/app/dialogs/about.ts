a22445614163c952cc8c1f973ad447cad5be3ca6
James Addison <james@reciperadar.com>
2022-12-29 16:32:30 +0000

    Sankey diagram rendering: use SVG rotation instead of requesting a vertically-aligned D3 sankey diagram

--- src/app/dialogs/about.ts
+++ src/app/dialogs/about.ts
@@ -13,8 +13,7 @@ declare global {
 }
 window.jQuery = $;
 
-// TODO: import d3-sankey without referencing the node_modules directory
-import { sankeyTop } from '../../../node_modules/d3-sankey/src';
+import { sankey as sankeyInstance, sankeyLinkHorizontal } from 'd3-sankey';
 import * as data from './about-diagram.json';
 
 export {};
@@ -32,25 +31,25 @@ $('#about-modal').on('shown.bs.modal', function() {
 function renderDiagram() {
   const container = $('#about-modal div.modal-body');
   const margin = { top: 5, right: 5, bottom: 5, left: 5 };
-  const width = container.width() - margin.left - margin.right;
-  const height = 300 - margin.top - margin.bottom;
+  const width = container.width();
+  const height = 300 + margin.top + margin.bottom;
 
   const svg = d3
       .select('#about-modal div.chart')
       .append('svg')
-      .attr('width', width + margin.left + margin.right)
+      .attr('width', width)
       .attr('height', height + margin.top + margin.bottom)
       .append('g')
-      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+      .attr('transform', 'rotate(90 ' + (height / 2) + ' ' + (height) + ') translate(' + (-height / 2) + ',' + (- (margin.left + margin.right)) + ')');
 
-  const sankey = sankeyTop()
+  const sankey = sankeyInstance()
       .size([width, height])
       .nodeId(d => d.name)
       .nodeWidth(20)
       .nodePadding(10)
       .extent([
           [0, 5],
-          [width, height - 5]
+          [height, width - 5]
       ]);
 
   const graph = sankey(data);
@@ -73,7 +72,7 @@ function renderDiagram() {
       .data(graph.links)
       .join('g')
       .append('path')
-      .attr('d', sankey.linkShape())
+      .attr('d', sankeyLinkHorizontal())
       .attr('class', 'link')
       .attr('stroke-width', 16)
       .append('title').text(d => `${d.source.name} →  ${d.target.name}`);
@@ -87,5 +86,6 @@ function renderDiagram() {
       .attr('y', d => (d.y1 + d.y0) / 2)
       .attr('dy', '0.2em')
       .attr('text-anchor', 'middle')
+      .attr('transform', d => 'rotate(-90 ' + ((d.x0 + d.x1) / 2) + ' ' + ((d.y1 + d.y0) / 2) + ')')
       .text(d => d.name);
 }
