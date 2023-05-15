28cc26b6b3c5009db73b0cf6a4c0761198b3df7d
James Addison <james@reciperadar.com>
2023-05-15 13:53:24 +0100

    Fixup: relocate 'bootstrap-table' initialization import

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,5 +1,3 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-table/dist/bootstrap-table.css';
 import 'select2/dist/css/select2.css';
 import 'tablesaw/dist/stackonly/tablesaw.stackonly.css';
 
@@ -20,6 +18,10 @@ import './views/components/recipe-list.css';
 import './main.css';
 
 import './database';
+
+import 'bootstrap-table';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-table/dist/bootstrap-table.css';
 
 import { recipeFormatter, rowAttributes } from './views/components/recipe-list';
 export { recipeFormatter, rowAttributes };
