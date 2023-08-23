69cf0a7c39cbeefd187afeaa98f1746a43eaadbd
James Addison <james@reciperadar.com>
2023-08-23 19:20:58 +0100

    recipe-list component: only call show() function when data has been received

--- src/app/views/components/recipe-list.ts
+++ src/app/views/components/recipe-list.ts
@@ -236,7 +236,7 @@ function bindPostBody(selector: string) : void {
     $(this).find('button.add-recipe').each((_, button) => {
       $(button).on('click', () => { getRecipe(button).then(addRecipe).then(updateRecipeState); });
     });
-    if (data) {
+    if (data && data.length) {
       $(this).parents('div.recipe-list').show();
     }
 
