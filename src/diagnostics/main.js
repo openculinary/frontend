548ca3ed93c8761ce9766ecdaa0b265d64902bed
James Addison <james@reciperadar.com>
2024-02-05 16:02:55 +0000

    Update dependencies

--- src/diagnostics/main.js
+++ src/diagnostics/main.js
@@ -1,6 +1,7 @@
 import * as $ from 'jquery';
-import { create as jsondiffpatch, formatters as diffformatters } from 'jsondiffpatch';
-import 'jsondiffpatch/dist/formatters-styles/html.css'
+import * as jsondiffpatch from 'jsondiffpatch';
+import * as HtmlFormatter from 'jsondiffpatch/formatters/html'
+import 'jsondiffpatch/formatters/styles/html.css'
 
 function getRecipeId() {
   var urlParams = new URLSearchParams(window.location.hash.slice(1));
@@ -10,9 +11,9 @@ function getRecipeId() {
 $(function() {
   var recipeId = getRecipeId();
   $.ajax({url: `/diagnostics/recipes/${recipeId}`}).then(recipe => {
-    var jsondiff = jsondiffpatch();
+    var jsondiff = jsondiffpatch.create();
     var diff = jsondiff.diff(recipe.indexed, recipe.crawled);
-    $('#diff').html(diffformatters.html.format(diff, recipe.indexed));
+    $('#diff').html(HtmlFormatter.format(diff, recipe.indexed));
     eval($('#diff').find('script').html());
   });
 });
