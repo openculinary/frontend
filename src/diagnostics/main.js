cb83728fdafa48c25ca188618efb86053e712205
James Addison <james@reciperadar.com>
2024-08-12 18:52:13 +0100

    Revert "Update dependencies"
    This reverts commit 548ca3ed93c8761ce9766ecdaa0b265d64902bed.

--- src/diagnostics/main.js
+++ src/diagnostics/main.js
@@ -1,7 +1,6 @@
 import * as $ from 'jquery';
-import * as jsondiffpatch from 'jsondiffpatch';
-import * as HtmlFormatter from 'jsondiffpatch/formatters/html'
-import 'jsondiffpatch/formatters/styles/html.css'
+import { create as jsondiffpatch, formatters as diffformatters } from 'jsondiffpatch';
+import 'jsondiffpatch/dist/formatters-styles/html.css'
 
 function getRecipeId() {
   var urlParams = new URLSearchParams(window.location.hash.slice(1));
@@ -11,9 +10,9 @@ function getRecipeId() {
 $(function() {
   var recipeId = getRecipeId();
   $.ajax({url: `/diagnostics/recipes/${recipeId}`}).then(recipe => {
-    var jsondiff = jsondiffpatch.create();
+    var jsondiff = jsondiffpatch();
     var diff = jsondiff.diff(recipe.indexed, recipe.crawled);
-    $('#diff').html(HtmlFormatter.format(diff, recipe.indexed));
+    $('#diff').html(diffformatters.html.format(diff, recipe.indexed));
     eval($('#diff').find('script').html());
   });
 });
